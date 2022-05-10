0995564e65918fe126553a79cf3ff1b499fa6ef5
JanekKos2 <jan.pyra@student.pcez.pl>
2022-05-10 17:58:03 +0200

    1.4

---
+++ js/app.js
@@ -0,0 +1,9 @@
+buttom1=document.getElementById("buttom1");
+console.log(buttom1);
+
+buttom1.addEventListener("click",animation);
+
+function animation(e){
+    console.log(e.target);
+    buttom1.classList.add()
+}
