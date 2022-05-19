6eeea40c5a5c21ef514540f8d2000e9cf1cb33d4
JanekKos2 <jan.pyra@student.pcez.pl>
2022-05-19 18:58:11 +0200

    Wyszukiwanie

--- js/app.js
+++ js/app.js
@@ -1,45 +1,49 @@
 var pierwiastki = [] ;
-//console.log(wylosowany);
 var y = 0 ;
-container = document.getElementsByClassName("symbol");
-//console.log(container.length);
+
+container = document.getElementsByClassName("symbol") ;
+
 function randomElements(){
-for(x = 0; x < container.length; x++){
-  if(container[x].textContent != "DE" 
-  && container[x].textContent != "DEL" 
-  && container[x].textContent != "57-71" 
-  && container[x].textContent != "89-103"  ){
-      pierwiastki[y] = container[x].textContent ;
-      y++ ;
-  }
-}
-for(x = 0; x < pierwiastki.length; x++){
-//console.log("x:"+x+","+pierwiastki[x]);
-}
 
-random = Math.floor(Math.random() * pierwiastki.length) ;
-console.log(pierwiastki[random]);
-
-for(x = 0 ; x < container.length ; x++){
-  if(container[x].textContent == pierwiastki[random]){
-     wylosowany = container[x] ;
+//console.log(container.length) ;
+for(x = 0; x<container.length; x++){
+    if(container[x].textContent != "DE" 
+    && container[x].textContent != "57-71" 
+    && container[x].textContent != "89-103"){
+        pierwiastki[y] = container[x].textContent ;
+        y++ ;
     }
 }
 
-console.log(wylosowany);
-wylosowany = wylosowany.parentNode ;
-wylosowany.setAttribute("class","checked");
+for(x = 0; x<pierwiastki.length; x++){
+//    console.log("x:"+x +"," +pierwiastki[x]) ;
 }
 
-const easy = document.getElementById("easy");
-const medium = document.getElementById("medium");
-const hard = document.getElementById("hard");
+random = Math.floor(Math.random() * pierwiastki.length) ;
+console.log(pierwiastki[random]) ;
 
-easy.addEventListener('click',gameStart);
-medium.addEventListener('click',gameStart);
-hard.addEventListener('click',gameStart);
+for(x = 0; x<container.length; x++){
+    if(container[x].textContent == pierwiastki[random]){
+        wylosowany = container[x] ;
+    }
+}
+console.log(wylosowany) ;
+wylosowany = wylosowany.parentNode ;
+
+wylosowany.setAttribute("class","checked")
+console.log(wylosowany) ;
+}
+
+const easy = document.getElementById("easy") ;
+const normal = document.getElementById("normal") ;
+const hard = document.getElementById("hard") ;
+
+easy.addEventListener('click',gameStart) ;
+normal.addEventListener('click',gameStart) ;
+hard.addEventListener('click',gameStart) ;
 
 function gameStart(e){
-  console.log(e.target.id);
-  randomElements();
+    console.log(e.target.id) ;
+    randomElements() ;
 }
+
