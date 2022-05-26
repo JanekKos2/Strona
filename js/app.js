423acc3a50aed4c8be7c6f8aff1d8306885ea507
JanekKos2 <jan.pyra@student.pcez.pl>
2022-05-26 19:18:10 +0200

    ruletka

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 var elementsTable = [] ;
-var elementChosed = [] ;
+var elementChoosed = [] ;
 var answer = "";
 var y = 0 ;
 var k=0 ;
+var scor = 0 ;
 var point = 0;
-var timer = 0;
-var countDownDate = new Date().getTime()+1000*120;
 container = document.getElementsByClassName("symbol");
 for(x = 0; x < container.length; x++){
   if(container[x].textContent != "DE"
@@ -16,23 +15,21 @@ for(x = 0; x < container.length; x++){
       y++ ;
   }
 }
-
+document.getElementById("score").className = "hide"
 function randomElements(){
-if(elementChosed.length < elements.length){
-randomNumber = Math.floor(Math.random() * elements.length) ;
-answer = elements[randomNumber][1];
-randomElement = elementsTable[randomNumber].parentNode ;
-if (elementChosed.includes(randomNumber)){
+if(elementChoosed.length < elements.length){
+random = Math.floor(Math.random() * elements.length) ;
+answer = elements[random][1];
+ele = elements[random][0];
+randomElement = elementsTable[random].parentNode ;
+if (elementChoosed.includes(random)){
        randomElements();
 } else {
 randomElement.setAttribute("class","checked");
-elementChosed.push(randomNumber);
+elementChoosed.push(random);
 }
 } else {
-  document.getElementById("statictic").className = "show btn btn-warning"
-  document.getElementById("inputAuto").className = "hide"
-  document.getElementById("end").className = "show"
-  clearInterval(timer)
+  
 }
 }
 
@@ -45,16 +42,14 @@ medium.addEventListener('click',gameStart);
 hard.addEventListener('click',gameStart);
 
 function gameStart(e){
-
-  countDownDate = new Date().getTime()+1000*120;
-  timer = setInterval(countDown,1000);
   randomElements();
   document.getElementById("startButton").className = "hide"
   document.getElementById("inputAuto").className = "show"
   document.getElementById("resetButton").className = "show btn btn-info"
-  document.getElementById("points").className = "col-6 left padding0 show"
-  document.getElementById("timer1").className = "col-6 left padding0 show"
-  statistics.innerHTML = "<h1>Liczba Punktów: "+point+"</h1>"
+  document.getElementById("score").className = "show"
+  document.getElementById("timer").className = "show"
+  document.getElementById("score").innerHTML ="<div> Wynik: "+scor+"</div>"  ;
+  countDownDate = new Date().getTime()+1000*10*30;
 }
 
 function checkQuestion(event){
@@ -62,79 +57,72 @@ function checkQuestion(event){
     inputAuto = document.getElementById("inputAuto").value
     if(inputAuto == answer){
       randomElement.classList.remove("checked");
-      randomElement.classList.add("goodAnswer");
-      randomElement.classList.add("okres");
-      point++;
-      countDownDate += 10*1000;
-      if(point == 2){
-        document.getElementById("walterWhite").innerHTML = '<img src="../img/walter.png" alt="">';
-        animation();
-      } 
-      
-      else {
-        document.getElementById("walterWhite").innerHTML = "";
-      }
+      randomElement.classList.add("true");
+      randomElement.classList.add("pink");
+      scor++ ;
+      document.getElementById("score").innerHTML ="<div> Wynik: "+scor+"</div>"  ;
     }
     else {
       randomElement.classList.remove("checked");
-      randomElement.classList.add("badAnswer");
-      randomElement.classList.add("okres");
+      randomElement.classList.add("false");
+      randomElement.classList.add("pink");
     }
-    statistics.innerHTML = "<h1>Liczba Punktów: "+point+"</h1>";
     randomElements();
-    event.target.value = "";
+    document.getElementById("inputAuto").value = "";
   }
 }
 
 resetButton.addEventListener("click", resetGame);
 function resetGame(e){
-  for(x = 0; x < elements.length; x++){
-    elementsTable[x].parentNode.classList.remove("badAnswer");
-    elementsTable[x].parentNode.classList.remove("goodAnswer");
+  for(x = 0; x < elementsTable.length; x++){
+    elementsTable[x].parentNode.classList.remove("false");
+    elementsTable[x].parentNode.classList.remove("true");
     elementsTable[x].parentNode.classList.remove("checked");
-    elementsTable[x].parentNode.classList.remove("okresowy");
-    elementsTable[x].parentNode.classList.add("okresowy");
+    elementsTable[x].parentNode.classList.remove("pink");
+    elementsTable[x].parentNode.classList.add("pink");
   }
-  elementChosed = [] ;
+  elementChoosed = [] ;
   document.getElementById("startButton").className = "btn btn-secondary show";
   document.getElementById("inputAuto").className = "hide";
   document.getElementById("resetButton").className = "hide";
-  document.getElementById("points").className = "col-6 left padding0 hide";
-  document.getElementById("timer1").className = "col-6 left padding0 hide";
-  point = 0;
-  clearInterval(timer);
-  countDownDate = new Date().getTime()+1000*120;
-  timer = setInterval(countDown,1000);
-  statistics.innerHTML = "";
+  document.getElementById("score").className = "show"
+  document.getElementById("timer").className = "hide"
+  scor=0
+  document.getElementById("score").innerHTML =""  ;
 }
+//TIMER
+var countDownDate = new Date().getTime()+1000*10*30;
 
-function countDown(){
-// Set the date we're counting down to
+var x = setInterval(function() {
 
-
-  // Get today's date and time
   var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
+
   var distance = countDownDate - now;
-    
-  // Time calculations for minutes and seconds
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  var minutes = Math.floor((distance % (1000 * 10 * 30)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + ":" + seconds + "";
-    
-  // If the count down is over, write some text 
+
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+  if(scor == 1){
+    document.getElementById("chance").innerHTML = "Wybierz pierwiastek" ;
+    document.getElementsByClassName("checked").className = "pink"
+    document.getElementById("inputElem").className = "show"
+    inputElem = document.getElementById("inputElem").value
+}
+
   if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById("demo").innerHTML = "KONIEC";
-    resetGame();
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "KONIEC GRY";
   }
-}
+}, 1000);
+  function checkEle(event){
+    if(event.keyCode == 13){
+      inputElem = document.getElementById("inputElem").value
+      for(x= 0 ; x < elements.length ; x++) {
+        if(inputElem == elements[x][0]){
+              elementsTable[x].parentNote.className = "checked";
+        }
 
-// Update the count down every 1 second
-
-function animation(){
-  walterWhite.classList.add('animate__animated', 'animate__fadeInUpBig');
-}
+      }
+    }
+  }
